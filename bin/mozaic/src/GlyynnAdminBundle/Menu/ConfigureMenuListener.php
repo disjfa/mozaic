fc74b9e86f33fd0631a4dd55600f23c0a91b33ba
disjfa <disjfa@disjfa.nl>
2016-12-14 23:02:00 +0000

    added gulp file and action for deployment

--- bin/mozaic/src/GlyynnAdminBundle/Menu/ConfigureMenuListener.php
+++ bin/mozaic/src/GlyynnAdminBundle/Menu/ConfigureMenuListener.php
@@ -13,10 +13,5 @@ class ConfigureMenuListener
     public function onMenuConfigure(ConfigureMenuEvent $event)
     {
         $menu = $event->getMenu();
-        $submenu = $menu->addChild('Submenu', ['route' => 'glynn-admin-submenu'])->setExtra('icon', 'fa-paper-plane');
-        $submenu->addChild('Submenu 1', ['route' => 'glynn-admin-submenu1'])->setExtra('icon', 'fa-paper-plane');
-        $submenu->addChild('Submenu 2', ['route' => 'glynn-admin-submenu2'])->setExtra('icon', 'fa-paper-plane');
-        $submenu3 = $submenu->addChild('Submenu 3', ['route' => 'glynn-admin-submenu3'])->setExtra('icon', 'fa-paper-plane');
-        $submenu3->addChild('Sub-submenu 1', ['route' => 'glynn-admin-subsubmenu1'])->setExtra('icon', 'fa-paper-plane');
     }
 }
