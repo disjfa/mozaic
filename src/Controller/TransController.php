a05b24e5a7d7729e718d550ed332ee13090137cd
disjfa <disjfa@disjfa.nl>
2018-06-15 21:31:29 +0000

    Added footer, disables rounded

---
+++ src/Controller/TransController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class TransController extends Controller
+{
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @Route("/trans", name="trans_trans")
+     */
+    public function trans()
+    {
+        dump($this->translator->getCatalogue());
+        dump($this);
+        exit;
+    }
+}
